0e923603168f89f62de03a288d09b1907ad835a1
sarikachilagani <84078414+sarikachilagani@users.noreply.github.com>
2021-07-07 14:18:23 +0530

    Add files via upload
    added files

---
+++ ATM/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.io.IOException;
+
+public class Main extends OptionMenu {
+    static public void main(String args[]) throws IOException {
+        OptionMenu optionMenu =new OptionMenu();
+
+        optionMenu.getLogin();
+    }
+
+}
